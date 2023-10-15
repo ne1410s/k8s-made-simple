3017c6eeb06aff8eda0f8953287a6f903960b20e
ne1410s <pgj646@gmail.com>
2023-10-15 21:49:40 +0100

    loki working..

--- apps-source-code/FileMan/FileMan.Library.ClamAv/ClamAvScanner.cs
+++ apps-source-code/FileMan/FileMan.Library.ClamAv/ClamAvScanner.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text.Json;
 using FileMan.Business.Features.Av;
+using Microsoft.Extensions.Logging;
 using nClam;
 
 namespace FileMan.Av.ClamAv;
@@ -7,17 +8,19 @@ namespace FileMan.Av.ClamAv;
 public class ClamAvScanner : IAntiVirusScanner
 {
     private readonly IClamClient _client;
+    private readonly ILogger<ClamAvScanner> _logger;
 
-    public ClamAvScanner(IClamClient client)
+    public ClamAvScanner(IClamClient client, ILogger<ClamAvScanner> logger)
     {
         _client = client;
+        _logger = logger;
     }
 
     public async Task<bool> IsContentSafe(Stream content)
     {
         var scanResult = await _client.SendAndScanFileAsync(content);
         var resultJson = JsonSerializer.Serialize(scanResult);
-        Console.WriteLine(resultJson);
+        _logger.LogInformation("File scanned, length: {length}, result: {json}", content.Length, resultJson);
 
         var retVal = scanResult.Result switch
         {
